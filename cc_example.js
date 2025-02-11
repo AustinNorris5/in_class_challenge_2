d1de93e6dd55ae5e2736ad3b8b788acb632525d0
AustinNorris5 <ajnorris@usf.edu>
2025-02-11 16:32:53 -0500

    Task 3

--- cc_example.js
+++ cc_example.js
@@ -11,15 +11,24 @@ let products = [
 //console.log(getPoroductsByCategory(products, "Electronics"));
 
 //Task 2
-function applyDiscount(products, discountRate) {
-    return products.map(product => ({
-        ...product,
-        price: product.price - (product.price * discountRate)
-    }
-    ))
-}
+// function applyDiscount(products, discountRate) {
+//     return products.map(product => ({
+//         ...product,
+//         price: product.price - (product.price * discountRate)
+//     }
+//     ))
+// }
 
 console.log(applyDiscount(products, .1));
 console.log("*".repeat(20));
 console.log(products);
+
+//Task 3
+let sales = [250, 400, 150, 900, 1200]
+
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale)=>total+sale, 0)
+}
+
+console.log(calculateTotalRevenue(sales))
 
