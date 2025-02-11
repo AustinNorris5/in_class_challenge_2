2457faff67c7db9f3663e425a5e8dce9db5b72a4
AustinNorris5 <ajnorris@usf.edu>
2025-02-11 16:27:34 -0500

    Completed Task 2

--- cc_example.js
+++ cc_example.js
@@ -3,3 +3,23 @@ let products = [
     { name: "Laptop", price: 1200, category: "Electronics" },
     { name: "Phone", price: 800, category: "Electronics" }
 ];
+
+//function getPoroductsByCategory(products, category) {
+    //return products.filter(product => product.category === category);
+//}
+
+//console.log(getPoroductsByCategory(products, "Electronics"));
+
+//Task 2
+function applyDiscount(products, discountRate) {
+    return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate)
+    }
+    ))
+}
+
+console.log(applyDiscount(products, .1));
+console.log("*".repeat(20));
+console.log(products);
+
